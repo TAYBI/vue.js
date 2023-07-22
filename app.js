037ec08b48a4d6c152e1a809bd04d73c057a7398
Bilal <taybib4@gmail.com>
2023-07-22 12:46:31 +0100

    simple increment page app

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            message: 'Vue js, ma bright coding b darija',
-            title: ' page Title',
-            imgSrc: 'https://cdn.vectorstock.com/i/preview-1x/78/59/happy-grin-emoji-instant-messaging-icon-imag-vector-17067859.jpg',
-            htmlCode: '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, harum.</p>'
+            counter: 0
         }
     },
     methods: {
-        welcomeMessage() {
-            return `salam bdina net3almo ${this.message}`
-        }
+        incCounter(step = 1) {
+            this.counter += step;
+        },
+        DecCounter(step = 1) {
+            this.counter -= step;
+        },
     }
 })
 
