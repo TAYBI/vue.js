ad747c51e890a73b972a9b8a7c4c8cfdcf26439a
Bilal <taybib4@gmail.com>
2023-07-25 15:10:46 +0100

    load post details

--- vue-routing/src/views/blog/BlogDetails.vue
+++ vue-routing/src/views/blog/BlogDetails.vue
@@ -1,15 +1,46 @@
 <template >
-    <div class="card text-white bg-secondary mb-3" style="max-width: 20rem;">
-        <div class="card-header">{{ id }} - {{ slug }}</div>
-        <div class="card-body">
-            <h4 class="card-title">Secondary card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
+    <div v-if="blog" class="jumbotron alert alert-secondary">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h1 class="display-4" style="font-weight: bold;">{{ blog.strMeal }}</h1>
+                <img class="img-fluid mb-3" style="max-height: 300px; height: auto; width: 100%;" :src="blog.strMealThumb"
+                    alt="Card image cap">
+                <p class="lead">{{ blog.strInstructions }}</p>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="row mb-3">
+                    <div class="col-4">
+                        <span class="badge bg-dark">{{ blog.strArea }}</span>
+                    </div>
+                    <div class="col-4">
+                        <span class="badge bg-danger">{{ blog.strTags }}</span>
+                    </div>
+                    <div class="col-4">
+                        <span class="badge bg-warning">{{ blog.strCategory }}</span>
+                    </div>
+                </div>
+                <hr class="my-4">
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                </p>
+            </div>
         </div>
     </div>
 </template>
 <script>
 export default {
     props: ['id', 'slug'],
+    data() {
+        return {
+            blog: null
+        }
+    },
+    async mounted() {
+        const id = this.$route.params.id;
+        const response = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+        const data_ = await response.json();
+        this.blog = data_.meals[0];
+        console.log(this.blog);
+    },
 }
 </script>
--- vue-routing/src/views/blog/BlogView.vue
+++ vue-routing/src/views/blog/BlogView.vue
@@ -27,7 +27,6 @@ export default {
   },
   async mounted() {
 
-
     const response = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=");
     const data = await response.json();
     const blogs_ = data.meals
