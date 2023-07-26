ffb22f01a9eb713ba66e2a4aeb46c5f0163def5e
Bilal <taybib4@gmail.com>
2023-07-26 13:33:39 +0100

    get rid of vue tify

---
+++ vue-firebase/src/components/SideBare.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <VNavigationDrawer rail> 
+    
+        <VList>
+          <VListitem
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            title="Sandra Adams"
+            subtitle="sandra_a88@gmailcom"
+          ></VListitem>
+        </VList>
+
+    </VNavigationDrawer>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
