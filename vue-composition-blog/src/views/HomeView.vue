34c62fb05fdf0eb1b7b05c6b8c105c575544045f
Bilal <taybib4@gmail.com>
2023-07-26 09:59:02 +0100

    ll

--- vue-composition-blog/src/views/HomeView.vue
+++ vue-composition-blog/src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">
     <h1 ref="header">{{ text.text }}</h1>
-    <button @click="saySalam">Salam</button>
+    <!-- <button @click="saySalam">Salam</button> -->
+    <input v-model="search" type="text">
+    <P v-for="course in searchResult">{{ course }}</P>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from 'vue';
+import { computed, reactive, ref, watch, watchPostEffect } from 'vue';
 
 
 export default {
@@ -17,14 +19,33 @@ export default {
     let text = reactive({ text: "Home" })
     let header = ref(null);
 
-
+    const search = ref('')
+    const courses = ref(['js', 'html', 'css', 'c#', 'python', 'C'])
     //Methods
+
+    // computed
+    const searchResult = computed(() => {
+      return courses.value.filter((course) => {
+        return course.includes(search.value)
+      })
+    })
+
+    // watch 
+    watch(search, () => {
+      console.log('I see you: ', search.value);
+    })
+
+    // watchPostEffect
+    watchPostEffect(() => {
+      console.log('I see you - watch effect: ', search.value);
+    })
+
     const saySalam = () => {
       // header.value.textContent = 'changed'
       text.text = "second home"
       console.log(text);
     }
-    return { text, saySalam, header }
+    return { text, saySalam, header, courses, searchResult, search }
   }
 }
 </script>
