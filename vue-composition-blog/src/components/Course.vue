94e8174412c43e7f6d7e619d6ee223754e8ab0e4
Bilal <taybib4@gmail.com>
2023-07-26 10:23:58 +0100

    understund life sycle hooks

--- vue-composition-blog/src/components/Course.vue
+++ vue-composition-blog/src/components/Course.vue
@@ -1,15 +1,34 @@
 <template lang="">
-        <h2>{{course.title}}</h2>
-        <p>{{extract}}</p>
+        <h2 @click="show = !show">{{course.title}}</h2>
+        <p v-if="show">{{extract}}</p>
 </template>
 <script>
-import { computed } from 'vue';
+import { computed, onMounted, onUnmounted, onUpdated, ref } from 'vue';
 
 export default {
     props: ['course'],
     setup(props) {
         const extract = computed(() => props.course.content.substring(0, 50) + '...');
-        return { extract };
+
+
+        // LIFE SYCLE HOOKS
+        onMounted(() => {
+            console.log('onMounted');
+        })
+
+        onUnmounted(() => {
+            console.log('onUnmounted');
+        })
+
+        onUpdated(() => {
+            console.log('onUpdated');
+        })
+
+        let show = ref(false)
+
+        return { extract, show };
+
+
     },
 }
 </script>
