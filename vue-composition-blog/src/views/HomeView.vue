b5af49f7c85bd3b566940b4104bdf6ffdf8a29be
Bilal <taybib4@gmail.com>
2023-07-26 09:32:23 +0100

    learn about reacive

--- vue-composition-blog/src/views/HomeView.vue
+++ vue-composition-blog/src/views/HomeView.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="home">
-    <h1 ref="header">{{ text }}</h1>
+    <h1 ref="header">{{ text.text }}</h1>
     <button @click="saySalam">Salam</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 
 export default {
   name: 'HomeView',
   setup() {
     //data
-    let text = ref("Home")
-
+    // let text = ref("Home")
+    let text = reactive({ text: "Home" })
     let header = ref(null);
 
 
     //Methods
     const saySalam = () => {
       // header.value.textContent = 'changed'
-      text.value = "second home"
+      text.text = "second home"
       console.log(text);
     }
     return { text, saySalam, header }
