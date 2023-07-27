843d38d19f7e2209cc5b9176d623699735c4ee09
Bilal <taybib4@gmail.com>
2023-07-27 12:11:11 +0100

    start with vuex stae managment

---
+++ vuex-basics/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    counter: 5,
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
